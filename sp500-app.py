247d456bfd36a7bf6897ed791cfb3bcfa23dc516
hasle1975 <38669079+Asatradingsoftware@users.noreply.github.com>
2022-09-15 17:06:41 +0700

    Update sp500-app.py

--- sp500-app.py
+++ sp500-app.py
@@ -9,3 +9,16 @@ st.title('AEGIST TL Quant Macro Alpha')
 
 st.sidebar.header('User Input Features')
 
+tab1, tab2, tab3, tab4 = st.tabs(["Results", "Portfolio", "Drawdown", "Probability & Correlation"])
+
+with tab1:
+    st.subheader("Results for AEGIS TL Quant Macro Alpha")  
+    
+with tab2:
+    st.subheader("Results for AEGIS TL Quant Macro Alpha")  
+    
+with tab3:
+    st.subheader("Results for AEGIS TL Quant Macro Alpha")  
+    
+with tab4:
+    st.subheader("Results for AEGIS TL Quant Macro Alpha")      
